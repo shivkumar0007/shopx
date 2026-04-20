a2ac9532fbe854f98e6bce785b0a70e5d0f4a214
Shiv <186371286+shivkumar0007@users.noreply.github.com>
2026-04-20 11:36:13 +0530

    backend live api

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import toast from "react-hot-toast";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://shopx-mmye.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL
